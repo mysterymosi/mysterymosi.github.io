9247aa467ab61466959c691379121431d0a3cf7b
mysterymosim <okimmosimiracle@gmail.com>
2020-11-16 23:41:54 +0000

    added more pages

--- assets/js/on-scroll.js
+++ assets/js/on-scroll.js
@@ -11,21 +11,45 @@ $(document).ready(function() {
   
       return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
     }
+
+    function isElementInViewport(el) {
+      // special bonus for those using jQuery
+      if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+      }
+      var rect = el.getBoundingClientRect();
+      return (
+        (rect.top <= 0
+          && rect.bottom >= 0)
+        ||
+        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+        ||
+        (rect.top >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+      );
+    }
     // If element is scrolled into view, fade it in
     $(window).scroll(function() {
       $('.scroll-animations .animate__animated').each(function() {
-        if (isScrolledIntoView(this) === true) {
+        if (isElementInViewport(this) === true) {
           $(this).addClass('animate__fadeInUp');
         }
       });
       $('.fadeRight-animations .animate__animated').each(function() {
-        if (isScrolledIntoView(this) === true) {
+        if (isElementInViewport(this) === true) {
           $(this).addClass('animate__fadeInLeft');
         }
       });
       $('.fadeLeft-animations .animate__animated').each(function() {
-        if (isScrolledIntoView(this) === true) {
+        if (isElementInViewport(this) === true) {
           $(this).addClass('animate__fadeInRight');
+        }
+      });
+      $('.flipInY-animations .animate__animated').each(function() {
+        if (isElementInViewport(this) === true) {
+          $(this).addClass('animate__flipInY');
+          $(this).css("opacity", 1);
         }
       });
     });
